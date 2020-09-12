48d27c9bf950c6877df6d3ee69050b2102d50f94
Harish Yerra <harish.yerra@duke.edu>
2020-09-11 23:26:51 -0400

    Initial Commit

---
+++ UpBeat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UpBeat
+//
+//  Created by Harish Yerra on 9/11/20.
+//  Copyright © 2020 UpBeat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
