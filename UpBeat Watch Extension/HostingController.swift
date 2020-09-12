6e2bf2ecaff48bcc7bb435f2fc73063d418b1265
Harish Yerra <harish.yerra@duke.edu>
2020-09-11 23:35:25 -0400

    Setup watch target.

---
+++ UpBeat Watch Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  UpBeat Watch Extension
+//
+//  Created by Harish Yerra on 9/11/20.
+//  Copyright © 2020 UpBeat. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
